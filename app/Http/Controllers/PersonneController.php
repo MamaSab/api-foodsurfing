67154258dc0a88262413c4ba119d3554484328d4
SabApprenante <SabApprenante>
2018-09-19 16:30:24 +0200

    commentairesDates

--- app/Http/Controllers/PersonneController.php
+++ app/Http/Controllers/PersonneController.php
@@ -20,7 +20,30 @@ class PersonneController extends Controller
 
     public function repas(Request $request, $id)
     {
+        // date_default_timezone_set('Europe/Paris');
+
+        // $dateUrl = $request->get('date');
+        // dd($dateUrl);
+        // $dateTime = new \DateTime($dateUrl); 
+        // dd($dateString);
+        // $dateString = $dateTime;
+            
+        // $dateString = $dateTime->format('Y-m-d H:i:s');
+        // dd(date('Y-m-d H:i:s', $date));
+        // $d1 = new \DateTime('2018-09-18 13:39:35');
+        
+        // $myTs=new \DateTime();
+        // $myTs->setTimestamp($request->input('date'));
+
+        // dd($myTs->format('Y-m-d H:i:s'));
+        
+        // $myDate=$myTs->format('Y-m-d H:i:s');
+
+        // $results = app('db')->select("SELECT * FROM repas ");
         $results = app('db')->select("SELECT * FROM repas WHERE personnes_idPersonnes = " . $id . " AND dateRepas >= '" . $request->input('date') . "'");
+     //  dd("SELECT * FROM repas WHERE personnes_idPersonnes = " . $id . " AND dateRepas >= '" . $myDate. "'");
+        
+        
         return $results;
     }
 
